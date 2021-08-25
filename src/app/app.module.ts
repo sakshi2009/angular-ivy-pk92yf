dbcb7ee2b62a6f9e416932f66155df3db32e7bab
sakshi2009 <sakshiandani20@gmail.com>
2021-08-25 16:32:08 +0530

    Printing data added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginComponent } from './login/login.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule,
+     ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path:'', component: LoginComponent},
+      {path:'products', component: ProductListComponent}
+    ])],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    LoginComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
